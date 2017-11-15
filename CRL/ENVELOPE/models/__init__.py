cda04aec4d3329ec4d3876cfe5db658dc0479d04
Runzhe Yang <runzheyang@icloud.com>
2017-11-15 14:39:51 -0500

    add cnn

--- CRL/ENVELOPE/models/__init__.py
+++ CRL/ENVELOPE/models/__init__.py
@@ -1,10 +1,11 @@
 from linear import EnvelopeLinearCQN
+from cnn import EnvelopeCnnCQN
 
 def get_new_model(name, state_size, action_size, reward_size):
 	if name == 'linear':
 		return EnvelopeLinearCQN(state_size, action_size, reward_size)
-	if name == 'linear_large':
-		return EnvelopeLinearLargeCQN(state_size, action_size, reward_size)
+	if name == 'cnn':
+		return EnvelopeCnnCQN(state_size, action_size, reward_size)
 	else:
 		print "model %s doesn't exist."%(name)
 		return None
