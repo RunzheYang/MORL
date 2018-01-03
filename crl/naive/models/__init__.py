fd06836b256b4a2cec42d72fef523cbfaf87e983
Runzhe Yang <runzheyang@icloud.com>
2018-01-03 16:55:27 -0500

    linear_ext

--- crl/naive/models/__init__.py
+++ crl/naive/models/__init__.py
@@ -1,9 +1,12 @@
 from .linear import NaiveLinearCQN
+from .linear_ext import NaiveLinearCQN2
 
 
 def get_new_model(name, state_size, action_size, reward_size):
     if name == 'linear':
         return NaiveLinearCQN(state_size, action_size, reward_size)
+    elif name == 'linear_ext':
+        return NaiveLinearCQN2(state_size, action_size, reward_size)
     else:
         print("model %s doesn't exist." % (name))
         return None
